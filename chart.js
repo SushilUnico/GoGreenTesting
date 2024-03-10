dbd10939530eb9af17e21659362bcf49b38419b9
SushilUnico <162325433+SushilUnico@users.noreply.github.com>
2024-03-11 02:49:38 +0530

    Update chart.js

--- chart.js
+++ chart.js
@@ -7034,7 +7034,7 @@
                     let g = t.querySelector(A["chart-toggle"]);
                     g && q(g, "change", ()=>{
                             var w;
-                            console.log(w);
+                            console.log(w.textContent);
                             switch ((w = g.firstChild) == null ? void 0 : w.textContent){
                                 case "By region":
                                     k(!0);
