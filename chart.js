dd740285320425be7c4e4e8a3c2901a278296fe1
SushilUnico <162325433+SushilUnico@users.noreply.github.com>
2024-03-12 19:51:38 +0530

    Update chart.js

--- chart.js
+++ chart.js
@@ -6770,7 +6770,7 @@
                 let f = (()=>{
                         let g = document.createElement("div");
                         let vi = document.querySelector('[data-chart-element="search"]') 
-                        window.onscroll(event) => {g.style.opacity = "0"};
+                        window.onscroll(event) => {g.style.opacity = "0";};
                         vi.addEventListener("change", (event) => {
                                g.style.opacity = "1";
                         });  
