6bbbff694cf0ff99d110cf07cceb8a324fe18db3
SushilUnico <162325433+SushilUnico@users.noreply.github.com>
2024-03-14 13:29:33 +0530

    Update chart.js

--- chart.js
+++ chart.js
@@ -7029,6 +7029,20 @@
                         console.log("Chart file loaded: calling API's now");
                         if(window.location.pathname == '/' || window.location.pathname == "/profile-creation") {
                             // document.querySelector('a[wized="profile_filters_reset"]').click();
+                                fetch(`${Br}/table`, {
+    method: "POST",
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${Fr}`
+    },
+    body: JSON.stringify({})
+}).then(
+                                        data => {
+    // Handle the JSON data
+    console.log(data); // Or do whatever you need with the response data
+}
+)
                                 console.log("This is been clicked")
                         } else {
                             // document.querySelector('.filters_submit-button').click(); // TODO: instead of triggering click event listen an event from wized that filters are loaded
