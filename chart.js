b3d150874e9ee05c969a5c599a3fd6d07b5f845d
SushilUnico <162325433+SushilUnico@users.noreply.github.com>
2024-03-24 03:22:15 +0530

    Update chart.js

--- chart.js
+++ chart.js
@@ -4788,6 +4788,7 @@
                             break;
                         case 'CurrentEnergyConsumptionLower':
                             if (last_applied_filter && last_applied_filter[key] === t[key]
+                                console.log("tes", last_applied_filter['CurrentEnergyConsumptionUpper']);
                                 && last_applied_filter['CurrentEnergyConsumptionUpper'] === t['CurrentEnergyConsumptionUpper']) {
                                 return
                             } else {
