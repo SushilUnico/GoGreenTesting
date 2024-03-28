9350ed0c9f9bbb699b5a03c86794cfac2fbf5c38
SushilUnico <162325433+SushilUnico@users.noreply.github.com>
2024-03-28 18:08:17 +0530

    Update chart.js

--- chart.js
+++ chart.js
@@ -4778,7 +4778,7 @@
                         const current_heat_consumption_to = document.getElementById('current_heat_consumption_to-2');
                         (current_heat_consumption_from.value == -10 && current_heat_consumption_to.value == 313) ? null : 
                                 payload['energy_consumption_current'] = {
-                                    lower: (Number(t[key]) == 313) ? 312274 : (Number(t[key]) * 1000),
+                                    lower: Number(t[key]) * 1000,
                                     higher: (Number(t['CurrentEnergyConsumptionUpper']) == 313) ? 312274 : (Number(t['CurrentEnergyConsumptionUpper']) * 1000)
                                 };
                             break;
