71bfd6cd38497349515d1ac22f151581d1268a21
SushilUnico <162325433+SushilUnico@users.noreply.github.com>
2024-04-29 15:54:30 +0530

    Update chart.js

--- chart.js
+++ chart.js
@@ -4799,13 +4799,13 @@
                                 };
                             break;
                         case 'IncomeDeprivationRangeElderLower':
-                        const income_deprivation_older_from  = document.getElementById('income_deprivation_older_from-2');
-                        const income_deprivation_older_to = document.getElementById('income_deprivation_older_to-2');
-                        (income_deprivation_older_from.value == 0 && income_deprivation_older_to.value == 1) ? null :
+                        const income_deprivation_olderFrom  = document.getElementById('income_deprivation_older_from-2');
+                        const income_deprivation_olderTo = document.getElementById('income_deprivation_older_to-2');
+                        (income_deprivation_olderFrom.value == 0 && income_deprivation_olderTo.value == 1) ? null :
                                 console.log('income_deprivation_older_from is passing');
                                 payload['idop_value'] = {
-                                    'lower': income_deprivation_older_from.value,
-                                    'higher': income_deprivation_older_to.value
+                                    'lower': income_deprivation_olderFrom.value,
+                                    'higher': income_deprivation_olderTo.value
                                 };
                             break;
                         case 'IncomeDeprivationRangeLower':
